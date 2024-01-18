080f9e45361a22d0e0124070b49a3de53814673d
Befrimon <poloskovnicita@yandex.ru>
2024-01-19 02:18:17 +0300

    Update swaylock to swaylock-pretty. Update clonedot.py

--- clonedot.py
+++ clonedot.py
@@ -1,8 +1,22 @@
-from os import system
+from os import system, walk
 
-copy_it = [".config", ".zshrc"]
+copy_files = [".zshrc"]
+copy_dirs = [".config"]
+ignore = ["btop", "dconf", "neofetch", "pulse"]
 HOME = "/home/gdev"
 GIT = "/home/gdev/Projects/dotfiles"
 
-for file in copy_it:
+for file in copy_files:
     system(f"yes | cp -rfi {HOME}/{file} {GIT}/")
+
+conf_dir = []
+for dir in copy_dirs:
+    for _, dirnames, _ in walk(f"{HOME}/{dir}"):
+        conf_dir.extend(dirnames)
+        break
+
+for dir in conf_dir:
+    if dir in ignore:
+        continue
+    system(f"yes | cp -rfi {HOME}/.config/{dir} {GIT}/.config/")
+
